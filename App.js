4aee3bb87efab3245764b877c93679ac4dd6caea
Neil <z5202dbc@gmail.com>
2020-05-18 15:34:29 +0800

    First commit of the example

--- App.js
+++ App.js
@@ -1,170 +1,25 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { Component } from "react";
+import { ScrollView } from "react-native";
+import SensorView from "./SensorView";
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+const axis = ["x", "y", "z"];
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-import {
-  accelerometer,
-  gyroscope,
-  setUpdateIntervalForType,
-  SensorTypes
-} from "react-native-sensors";
-import { map, filter } from "rxjs/operators";
-
-function RNsensortesting() {
-  // setUpdateIntervalForType(SensorTypes.Accelerometer, 400); // defaults to 100ms
-
-  const subscription = accelerometer
-  .pipe(map(({ x, y, z }) => x + y + z), filter(speed => speed > 20))
-  .subscribe(
-    speed => console.log(`You moved your phone with ${speed}`),
-    error => {
-      console.log("The sensor is not available");
-    }
-  );
-
-  setTimeout(() => {
-  // If it's the last subscription to accelerometer it will stop polling in the native API
-  subscription.unsubscribe();
-  }, 1000);
-}
-
-
-
-const App: () => React$Node = () => {
-
-  console.log('I am here!!!');
-
-  var data;
-
-  // const subscription = accelerometer.subscribe(({ x, y, z, timestamp }) =>
-  //   console.log({ x, y, z, timestamp })
-  // );
-
-  const subscription = accelerometer
-  .pipe(map(({ x, y, z }) => x + y + z), filter(speed => speed > 0))
-  .subscribe(
-    speed => console.log(`You moved your phone with ${speed}`),
-    error => {
-      console.log("The sensor is not available");
-    }
-  );
-
-  // console.log(subscription);
-
-  // setTimeout(() => {
-  //   // If it's the last subscription to accelerometer it will stop polling in the native API
-  //   subscription.unsubscribe();
-  //   }, 1000);
-
-
-
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text>123</Text>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.!!
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
+const availableSensors = {
+  accelerometer: axis,
+  gyroscope: axis,
+  magnetometer: axis,
+  barometer: ["pressure"]
 };
+const viewComponents = Object.entries(availableSensors).map(([name, values]) =>
+  SensorView(name, values)
+);
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <ScrollView>
+        {viewComponents.map((Comp, index) => <Comp key={index} />)}
+      </ScrollView>
+    );
+  }
+}
