4b0903fd247ae33be95f3844faac9acdfa443b49
Neil <z5202dbc@gmail.com>
2020-05-24 21:33:44 +0800

    clean testing codes

--- App.js
+++ App.js
@@ -1,50 +1,7 @@
 import React, { Component } from "react";
 import { ScrollView, Text, SafeAreaView, View } from "react-native";
 import SensorView from "./SensorView";
-
 import Geolocation from "@react-native-community/geolocation"
-
-
-// let position
-// var v = 10
-// var altitude
-
-// Geolocation.getCurrentPosition(info => showinfo(info));
-
-// function showinfo(info) {
-//   console.log(info.coords.latitude)
-//   altitude = info.coords.latitude
-//   console.log(altitude)
-//   alert(altitude)
-// }
-
-
-
-// class Animal { 
-//   constructor(name) {
-//     this.name = name;
-//   }
-
-//   speak() {
-//     console.log(this.name + ' makes a noise.');
-//   }
-// }
-// var a = new Animal('Dog')
-// a.speak()
-
-// class Dog extends Animal {
-//   speak() {
-//       console.log(this.name + ' barks.');
-//   }
-// }
-
-// var d = new Dog('Mitzie');
-
-// // 顯示 Mitzie barks.
-// d.speak();
-
-
-
 
 
 const axis = ["x", "y", "z"];
