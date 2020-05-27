bf374368e1dd149265708c0c436c9f13d9af98fc
Neil <z5202dbc@gmail.com>
2020-05-27 12:43:45 +0800

    回歸原始

--- App.js
+++ App.js
@@ -19,33 +19,11 @@ const viewComponents = Object.entries(availableSensors).map(([name, values]) =>
 export default class App extends Component {
   render() {
 
-    var altitude
-    var position
-
-    function showinfo(info) {
-      // console.log('*****Hello*****')
-      // console.log(typeof info)
-      console.log(info.coords.latitude)
-      altitude = info.coords.latitude
-      console.log(altitude)
-      // alert(altitude)
-      position = info
-    }
+    // Geolocation.getCurrentPosition(info => console.log(info));
     
-    Geolocation.getCurrentPosition(showinfo);
-    
-    alert(position)
-
     return (
       <SafeAreaView>
         <ScrollView>
-          <View style={{ height:30, justifyContent:'center', backgroundColor:'gray' }}>
-            {/* <Text style={{ fontSize:30 }}>Hi</Text> */}
-            <Text>{position}</Text>
-            {/* <Text>{position.coords.latitude}</Text>
-            <Text>{position.coords.longitude}</Text>
-            <Text>{position.timestamp}</Text> */}
-          </View>
           {viewComponents.map((Comp, index) => <Comp key={index} />)}
         </ScrollView>
       </SafeAreaView>
